356c8e5db27c624a792c304cbc16fe0c266c6188
muhammed94munshid <36299359+muhammed94munshid@users.noreply.github.com>
2018-02-14 15:52:15 +0530

    Add files via upload

---
+++ capital.py
@@ -0,0 +1,11 @@
+ch = (raw_input("Enter a character: "))
+
+
+if(ch=='A' or ch=='B' or ch=='C' or ch=='E' or ch =='D' or ch =='F' or ch =='G' or ch=='H'
+ or ch=='I' or ch=='J' or ch=='K' or ch=='L' or ch=='M' or ch=='N' or ch=='O' or ch=='P' or ch=='Q'or ch =='R'or ch =='S' or ch =='T' or ch =='U' or ch =='V' or ch =='W' or ch =='X' or ch =='Y 'or ch =='Z' ):
+
+    print("is capital")
+
+else:
+
+    print("is a small")
